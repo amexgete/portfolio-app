134b3ad3c2649ab7343e463ff00b318b43abe431
amexgete <amaregatie21@gmail.com>
2022-12-16 00:05:07 -0700

    first portfolio

---
+++ src/components/About.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div
+      name="about"
+      className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
+    >
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            About
+          </p>
+        </div>
+
+        <p className="text-xl mt-20">
+          i am information system students i  learn different program language in order to develop my sofware
+        </p>
+
+        <br />
+
+        <p className="text-xl">
+        i am information system students i  learn different program language in order to develop my sofware
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
