526045232fbe1b8c834c85fd022b0e417cf602f4
amexgete <amaregatie21@gmail.com>
2022-12-16 02:53:08 -0700

    first portt

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -16,7 +16,7 @@ const Contact = () => {
 
         <div className=" flex justify-center items-center">
           <form
-            action="https://getform.io/f/61c99527-2b15-42cf-9b55-ad37d2f7daa6"
+            action="amregatie21@gmail.com"
             method="POST"
             className=" flex flex-col w-full md:w-1/2"
           >
