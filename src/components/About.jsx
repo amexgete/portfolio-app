bba594df186740032df3a8d604644319724ff518
amaregatie <119925066+amexgete@users.noreply.github.com>
2024-02-21 11:46:35 +0300

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,7 +14,17 @@ const About = () => {
         </div>
 
         <p className="text-xl mt-20">
-     I am proficient in various programming languages such as HTML, CSS, JavaScript, and have experience working with popular frameworks like React and Angular.
+  Bachelor of Science in Information Systems (2019-2023) 
+I have 3.57 CGPA.
+I have scored an average of 77 in National Exit Exam
+University Of  Gondar.
+Hardware Troubleshooting: Adept at diagnosing and resolving hardware issues, ensuring the optimal functionality of computer Systems.
+Network Troubleshooting: Experienced in identifying and rectifying network-related challenges, ensuring seamless connectivity and communication.
+Software Troubleshooting: Proficient in debugging software issues and providing effective solutions to enhance software performance.
+Computer Maintenance: Skilled in maintaining computer systems, implementing upgrades, and ensuring overall system efficiency.
+Programming Languages: Well in Java, C++, C#, Python, CSS, HTML, React JS, JavaScript, and Django REST framework. Capable of developing and implementing solutions across a range of programming languages.
+Microsoft Office suites: Expertise in utilizing MS Word, Excel, PowerPoint, and internet applications for efficient documentation and presentation.
+Database management system (Oracle & MySQL).
         </p>
 
         <br />
