08b8b3199132c9d6dbc236542d18ebebedc59d65
amaregatie <119925066+amexgete@users.noreply.github.com>
2024-02-23 13:41:45 +0300

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -18,12 +18,12 @@ Bachelor of Science in Information Systems (2019-2023)
 I have 3.57 CGPA.
 I have scored an average of 77 in National Exit Exam
 University Of  Gondar.
-Hardware Troubleshooting: Adept at diagnosing and resolving hardware issues, ensuring the optimal functionality of computer Systems.
-Network Troubleshooting: Experienced in identifying and rectifying network-related challenges, ensuring seamless connectivity and communication.
-Software Troubleshooting: Proficient in debugging software issues and providing effective solutions to enhance software performance.
-Computer Maintenance: Skilled in maintaining computer systems, implementing upgrades, and ensuring overall system efficiency.
+Hardware Troubleshooting: 
+Network Troubleshooting: 
+Software Troubleshooting: 
+Computer Maintenance: 
 Programming Languages: Well in Java, C++, C#, Python, CSS, HTML, React JS, JavaScript, and Django REST framework. Capable of developing and implementing solutions across a range of programming languages.
-Microsoft Office suites: Expertise in utilizing MS Word, Excel, PowerPoint, and internet applications for efficient documentation and presentation.
+Microsoft Office suites: 
 Database management system (Oracle & MySQL).
         </p>
 
