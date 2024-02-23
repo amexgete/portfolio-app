4800aec00d3d3d741dacd9dcf112ff456846ea47
amaregatie <119925066+amexgete@users.noreply.github.com>
2024-02-23 14:25:26 +0300

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -15,15 +15,23 @@ const About = () => {
 
         <p className="text-xl mt-20">
 <pre></pre>Bachelor of Science in Information Systems (2019-2023) 
+<br/>
 I have 3.57 CGPA.
 I have scored an average of 77 in National Exit Exam
 University Of  Gondar.
+<br/>
 Hardware Troubleshooting: 
+<br/>
 Network Troubleshooting: 
+<br/>
 Software Troubleshooting: 
+<br/>
 Computer Maintenance: 
+<br/>
 Programming Languages: Well in Java, C++, C#, Python, CSS, HTML, React JS, JavaScript, and Django REST framework. Capable of developing and implementing solutions across a range of programming languages.
+<br/>
 Microsoft Office suites: 
+          <br/>
 Database management system (Oracle & MySQL).<pre/>
         </p>
 
