2839b69ad145f6aa7760cd6a5d2f08c793a7f575
amaregatie <119925066+amexgete@users.noreply.github.com>
2024-02-23 13:33:51 +0300

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,10 +12,7 @@ const Home = () => {
       <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
         <div className="flex flex-col justify-center h-full">
           <h2 className="text-4xl sm:text-7xl font-bold text-white">
-Bachelor of Science in Information Systems (2019-2023) 
-I have 3.57 CGPA.
-I have scored an average of 77 in National Exit Exam
-University Of  Gondar.
+I am Junior SoftWare Developer.
           </h2>
           <p className="text-gray-500 py-4 max-w-md">
 
