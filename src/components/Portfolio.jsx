4a4618cb592942ee7d96a3f8293897a51d2cd738
amaregatie <119925066+amexgete@users.noreply.github.com>
2024-02-24 17:03:41 +0300

    Update Portfolio.jsx

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -56,12 +56,7 @@ const Portfolio = () => {
                 className="rounded-md duration-200 hover:scale-105"
               />
               <div className="flex items-center justify-center">
-                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
-                  Demo
-                </button>
-                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
-                  Code
-                </button>
+              
               </div>
             </div>
           ))}
